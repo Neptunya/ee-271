82ae7081a9c25f0e39223788cfdf42d59d9a2377
ahamai <ahamai@uw.edu>
2023-04-14 12:52:34 -0700

    working on freds house returns, having issues getting 3-bit wire to work T-T

--- lab3a/DE1_SoC.sv
+++ lab3a/DE1_SoC.sv
@@ -12,58 +12,5 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	assign HEX3 = 7'b1111111;
 	assign HEX4 = 7'b1111111;
 	assign HEX5 = 7'b1111111;
-
-	Nordstrom_Returns(LEDR, SW);
 endmodule
 
-// Checks if a given object is discounted based on its UPC code.
-module Discount_Checker (D, U, P, C);
-	output logic D;
-	input logic U, P, C;
-
-	assign D = P | (U & C);
-endmodule
-
-// Checks if a given object has been stolen based on its UPC code and if it has a mark (M).
-module Stolen_Checker (S, U, P, C, M);
-	output logic S;
-	input logic U, P, C, M;
-
-	assign S = ~(P | M | ~(U | ~C));
-endmodule
-
-module Nordstrom_Returns (LEDR, SW);
-	output logic [9:0] LEDR;
-	input logic [9:0] SW;
-	logic D, S;
-
-	Discount_Checker dc1 (.D(D), .U(SW[9]), .P(SW[8]), .C(SW[7]));
-	Stolen_Checker sc1 (.S(S), .U(SW[9]), .P(SW[8]), .C(SW[7]), .M(SW[0]));
-
-	always_comb begin
-		LEDR[9] = D;
-		LEDR[0] = S;
-	end
-endmodule
-
-module tb_Nordstrom_Returns();
-logic [9:0] LEDR;
-logic [9:0] SW;
-
-	Nordstrom_Returns dut (.LEDR, .SW);
-
-	integer i;
-	initial begin
-		SW [0] = 1'b0;
-		for (i = 0; i < 8; i++) begin
-			SW[9:7] = i;
-			#10;
-		end
-
-		SW [0] = 1'b1;
-		for (i = 0; i < 8; i++) begin
-			SW[9:7] = i;
-			#10;
-		end
-	end
-endmodule
